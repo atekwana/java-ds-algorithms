75d9b0e75059bf9bee24b7a4dbd334a21bd3cfd5
atekwana <kaseyatekwana@protonmail.com>
2025-08-29 12:11:06 -0400

    Formatting code

--- SinglyLinkedList/src/SinglyLinkedList.java
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -16,7 +16,6 @@ public class SinglyLinkedList<T> {
         /**
          * 1. Create a new list node & pass the methods param as an arg
          * 2. always check if the head node is null
-         * 
          */
         ListNode<T> pointer = new ListNode<>(data); // ------------------------------> O(1)
 
@@ -107,7 +106,6 @@ public class SinglyLinkedList<T> {
         if (head == null) { // ------------------------------> O(1)
 
             System.out.println("SLL does not exist!"); // -> O(1)
-
             return;
 
         } else {
@@ -157,7 +155,6 @@ public class SinglyLinkedList<T> {
                 if (current.value.equals(target)) { // ------------------> O(1)
 
                     System.out.println(target + " is at node found at " + index); // ------------------> O(1)
-
                     return true; // ------------------> O(1)
 
                 }
@@ -169,7 +166,6 @@ public class SinglyLinkedList<T> {
         }
 
         System.out.print("node not found"); // ------------------> O(1)
-
         return false; // ------------------> O(1)
 
         // time complexity: O(n)
@@ -183,28 +179,27 @@ public class SinglyLinkedList<T> {
 
         /**
          * 1. Check if the head node is null; return immediately if the list is empty.
-         * 
          * Deleting the first node:
          * 2. If the deletion location is at the start of the linked list (location =
          * 0):
          * a. Move the head to the next node: head = head.next.
          * b. Reduce the size of the linked list by 1: size--.
          * c. If the linked list becomes empty (size == 0), set tail to null.
-         * 
+         *
          * Deleting the last node:
          * 3. If the deletion location is at or beyond the last node in the linked list:
          * a. Create a current node reference pointing to the head.
-         * 
+         *
          * Special case: If the list contains only one node:
          * b. If head.next is null, set both head and tail to null.
-         * 
+         *
          * General case: More than one node in the linked list:
          * c. Traverse the list using a while loop, stopping at the second-to-last node.
          * - The loop should run while current.next != null.
          * d. Set current.next to null, effectively removing the last node.
          * e. Update the tail to point to current.
          * f. Decrease the size of the linked list by 1.
-         * 
+         *
          * Deleting a node at a specific position:
          * 4. If the location is within the range of the linked list (not first or last
          * node):
