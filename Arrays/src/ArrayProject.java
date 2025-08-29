7add0fa8dbcd5f3c28022b1a805796f81a78dc77
atekwana <kaseyatekwana@protonmail.com>
2025-08-29 12:28:38 -0400

    Formatting code

--- Arrays/src/ArrayProject.java
+++ Arrays/src/ArrayProject.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class ArrayProject {
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        
 
         Scanner stdin = new Scanner(System.in);
 
         int days, sum = 0;
 
         System.out.print("How many days' temperature: ");
-
         days = stdin.nextInt();
 
         int[] temps = new int[days];
@@ -19,11 +18,9 @@ public class ArrayProject {
         for (int index = 0; index < days; index++) {
 
             System.out.print("Day " + (index + 1)+ "'s high temp: ");
-
             temps[index] = stdin.nextInt();
-
             sum += temps[index];
-            
+
         }
 
         double average = sum / days;
@@ -33,7 +30,7 @@ public class ArrayProject {
         int above = 0;
 
         for (int index = 0; index < temps.length; index++) {
-            
+
             if (temps[index] > average) {
 
                 above++;
@@ -46,6 +43,7 @@ public class ArrayProject {
 
         System.out.println(above + " days above average");
 
+        stdin.close();
 
     }
 
